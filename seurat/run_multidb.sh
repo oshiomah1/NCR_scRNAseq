b9e6cc979054e0ba5071f2139bb96e7a486874f8
Oshiomah Oyageshio <oshi@hive-dc-7-8-46.hive.hpc.ucdavis.edu>
2026-01-12 23:04:42 -0800

    scripts for running different sctype db in one object

---
+++ seurat/run_multidb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=multi_db
+#SBATCH --account=genome-center-grp
+#SBATCH --partition=high
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=120G
+#SBATCH --time=08:00:00
+#SBATCH --array=1-4
+#SBATCH --output=/quobyte/bmhenngrp/from-lssc0/projects/NCR_scRNAseq/scripts/seurat/process_with_wgs/logs/%x_%j.out
+#SBATCH --error=/quobyte/bmhenngrp/from-lssc0/projects/NCR_scRNAseq/scripts/seurat/process_with_wgs/logs/%x_%j.err
+#SBATCH --mail-user=oshiomah1234@gmail.com
+#SBATCH --mail-type=ALL
+
+module load conda
+eval "$($(command -v mamba) shell hook --shell bash)"
+mamba activate /quobyte/bmhenngrp/conda_envs/seuratt
+
+set -euo pipefail
+
+Rscript /quobyte/bmhenngrp/from-lssc0/projects/NCR_scRNAseq/scripts/seurat/process_with_wgs/multi_db_run.R \
+  /quobyte/bmhenngrp/from-lssc0/projects/NCR_scRNAseq/results/seurat/3_harmonize_batches_wgs/raw_merge_all_batches_harm_annotated_all_res12_pca35_noann.rds \
+  /quobyte/bmhenngrp/from-lssc0/projects/NCR_scRNAseq/results/ScType_multiDB_out
